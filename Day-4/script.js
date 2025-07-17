d75978076b4dbe05d99b073cc00d6e2757b24fc3
Aditya-Lokhande <adityalokhande8788@gmail.com>
2025-07-17 17:01:32 +0530

    git push

---
+++ Day-4/script.js
@@ -0,0 +1,23 @@
+alert("hello")
+
+alert("how are you")
+
+
+// 2 promt
+
+let name = prompt("enter your name")
+
+console.log(name)
+
+
+let first_1 = Number (prompt("enter first number"))
+
+let second_1 = Number (prompt("enter second number"))
+
+
+console.log(first_1 + second_1)
+
+
+// 3 confirm
+
+confirm("are you sure")
