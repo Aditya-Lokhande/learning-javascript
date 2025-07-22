2347de7fd116e61e3a7960a82de2fca2b2004508
Aditya-Lokhande <adityalokhande8788@gmail.com>
2025-07-22 19:34:10 +0530

    higher order and arrow function

--- DAY-15/script.js
+++ DAY-15/script.js
@@ -1,11 +1,85 @@
-let hello=()=>{
-    console.log("hello")
+//types of function
+
+//sum of numbers without function name
+
+let sum=function(a,b,c){
+    console.log(a+b+c);
 
 }
-hello()
+sum(20,45,50)
 
-let hello=(abc)=>{
-    console.log("hello")
+//even or odd function
+let evenoroddnumber=function(a){
+    if(a%2==0){
+        console.log("even");
+    }
+    else
+        console.log("odd")
+}
+
+let res1=evenoroddnumber(10)
+
+let res2=evenoroddnumber(25)
+
+
+//arrow function
+ let arrow=()=>{
+    console.log("arrow function");
+ }
+
+ //for sum 
+ let addition=(a,b)=>{
+    return(a+b)
+ }
+ console.log(addition(10,20));
+
+ //for subtraction
+ let subtraction=(a,b)=>{
+    return(a-b)
+ }
+ console.log(subtraction(10,20));
+
+ //for multiplication   
+ let multi=(a,b)=>{
+    return(a*b)
+
+ }
+ console.log(multi(10,20));
+
+ //for division
+ let div=(a,b)=>{
+    return(a/b)
+
+ }
+ console.log(div(10,20));
+
+//reverse function
+
+let arr=[10,30,80,6,70]
+const reversearr=()=>{
+    return arr.reverse()
 
 }
-hello(123)
+let res=reversearr(arr)
+console.log(res)
+
+//higher order function
+
+let sub = (a,b)=>{
+    console.log(a+b)
+
+}
+
+let operation =(anything)=>{
+    sub(10,20)
+
+}
+
+operation(sub)
+
+
+
+
+
+
+
