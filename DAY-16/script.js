dac39d6c63c109dd989599963a4dc844acdd8b9c
Aditya-Lokhande <adityalokhande8788@gmail.com>
2025-07-23 19:09:52 +0530

    document object model

---
+++ DAY-16/script.js
@@ -0,0 +1,16 @@
+let res=document.getElementsByTagName("h1")
+console.log(res)
+console.log(res[1])
+
+//get element by class name
+
+let res1=document.getElementsByClassName("third-dom")
+console.log(res1)
+
+// this is workein on any html tag 
+//now we use it on <p> means paragraph tag
+
+//add paragraph in html
+
+
+
